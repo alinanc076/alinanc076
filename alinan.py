cbc0a07b92fe48a971bae295c6ed3799ec534302
alinanc076 <163727521+alinanc076@users.noreply.github.com>
2024-04-28 11:38:44 +0300

    Create alinan.py

---
+++ alinan.py
@@ -0,0 +1,23 @@
+import discord
+import requests
+import os,random
+
+
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='.', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send('Selam! Ben bir botum!')
+
+bot.run("TOKEN)
